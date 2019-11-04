e3adf2f57c5411936eba2615e26684d37d7ccadc
k41zen <k41zen@live.co.uk>
2019-11-04 08:32:59 +0000

    Added sc-getrunning.py

---
+++ scripts/sc-getrunning.py
@@ -0,0 +1,19 @@
+from tenable.sc import TenableSC
+
+def main():
+    hostip = '<IP>'
+    username = '<username>'
+    password = '<password>'
+
+    sc = TenableSC(hostip)
+    sc.login(username, password)
+
+    for item in sc.get('scanResult').json()['response']['manageable']:
+        if 'Running' in item['status']:
+            print ('{id}: {name}'.format(**item))
+
+    sc.logout()
+
+if __name__ == '__main__':
+    main()
+
